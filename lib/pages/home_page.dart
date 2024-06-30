2a85e50e84c796274f55790d85bbe47baa4d9749
Chethiya2001 <chethiyabandaraherath@gmail.com>
2024-06-30 16:20:17 +0530

    first_commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,82 @@
+import 'package:cart_provider_app/data/product_data.dart';
+import 'package:cart_provider_app/models/product_model.dart';
+import 'package:cart_provider_app/pages/cart_page.dart';
+import 'package:cart_provider_app/pages/fav_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Product> products = ProductData().products;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product Page'),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.deepPurpleAccent,
+            heroTag: "favorite_button",
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => FavoritePage(),
+              ));
+            },
+            child: const Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            heroTag: "cart_button",
+            backgroundColor: Colors.deepPurpleAccent,
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => CartPage(),
+              ));
+            },
+            child: const Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final Product product = products[index];
+            return Card(
+              child: ListTile(
+                title: Row(
+                  children: [
+                    Text(product.title),
+                    const SizedBox(width: 50),
+                    //TODO: Add cart icon here
+                    const Text('0'),
+                  ],
+                ),
+                subtitle: Text("\$${product.price.toString()}"),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.favorite_border_outlined),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.shopping_cart_outlined),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
